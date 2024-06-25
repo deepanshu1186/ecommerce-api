0a945c26619093397d347792b756ff9a553e96fc
Deepanshu <deepashu.kakkar@gmail.com>
2024-06-25 23:31:42 +0530

    WIP

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,16 +5,16 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const getAdminProfile = async(req, res) => {
-    try{
+const getAdminProfile = async (req, res) => {
+    try {
         const admin_id = req.body.admin_id;
         const profile = await adminService.getAdminProfile(admin_id);
-        if(profile) {
+        if (profile) {
             res.json(
-                {'msg' : 'admin profile found.',profile})
+                { 'msg': 'admin profile found.', profile })
         } else {
             res.json({
-                'msg' : 'admin not found.'
+                'msg': 'admin not found.'
             })
         }
     } catch (error) {
@@ -58,10 +58,21 @@ const loginAdmin = async (req, res) => {
         }
 
         const token = jwt.sign({ id: admin.admin_id, username: admin.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
-        res.json({ token });
+
+        res.json({ token, 
+                    admin: { 
+                        id: admin.admin_id, 
+                        username: admin.username, 
+                        email: admin.email, 
+                        first_name: admin.first_name, 
+                        last_name: admin.last_name, 
+                        phone_number: admin.phone_number, 
+                        status: admin.status 
+                    }
+                });
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
 };
 
-module.exports = { getAdmins, createAdmin, loginAdmin,getAdminProfile };
+module.exports = { getAdmins, createAdmin, loginAdmin, getAdminProfile };
